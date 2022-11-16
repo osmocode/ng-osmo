2c83730ae55c2ae23067b4cbfeec2962af9c579d
osmocode <96887741+osmocode@users.noreply.github.com>
2022-11-17 00:34:44 +0100

    feat: gulp build:lib task

--- gulp/gulpfile.config.ts
+++ gulp/gulpfile.config.ts
@@ -1,13 +1,13 @@
-
-
-
-import { task, parallel } from 'gulp';
+import { task, parallel, dest, series } from 'gulp';
+const run = require('gulp-run');
 
 /*
   Import custom tasks
 */
 import { helpTask } from './tasks/help';
+import { execCommand } from './utils/command';
 
+task('build:lib', execCommand('ng build ng-osmo-lib'));
 
 task('help', helpTask());
 
