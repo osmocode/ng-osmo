b085a03f18cc46ec613798d00ed2e3a9f5cf68ce
osmocode <96887741+osmocode@users.noreply.github.com>
2022-11-16 13:35:04 +0100

    Fix showcase component link

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    NgoLayoutModule,
+    NgoLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
